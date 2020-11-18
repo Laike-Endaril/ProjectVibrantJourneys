14cd4bed5a3e9115bfac39d74993528c992cd6bb
Kyle Koder <silvertallon@gmail.com>
2020-11-18 10:32:36 -0800

    Ground cover is now instantly breakable (because it's loose), drops itself when broken, and...doesn't keep you from eating...

--- src/main/java/vibrantjourneys/blocks/BlockGroundCover.java
+++ src/main/java/vibrantjourneys/blocks/BlockGroundCover.java
@@ -1,11 +1,6 @@
 package vibrantjourneys.blocks;
 
-import java.util.Random;
-
-import javax.annotation.Nullable;
-
 import com.google.common.collect.ImmutableList;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
@@ -14,9 +9,6 @@ import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
-import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -26,68 +18,71 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import vibrantjourneys.util.IPropertyHelper;
 
+import javax.annotation.Nullable;
+import java.util.Random;
+
 public class BlockGroundCover extends Block implements IPropertyHelper
 {
-	public static final PropertyInteger MODEL = PropertyInteger.create("model", 0, 4);
-	private GroundcoverType groundcoverType;
-	
-	public BlockGroundCover(Material material, GroundcoverType type)
-	{
-		super(material);
-		this.setDefaultState(this.blockState.getBaseState().withProperty(MODEL, 0));
-		this.groundcoverType = type;
-		this.setHardness(0.1F);
-	}
-	
-	public GroundcoverType getGroundcoverType()
-	{
-		return this.groundcoverType;
-	}
-	
-	@Override
+    public static final PropertyInteger MODEL = PropertyInteger.create("model", 0, 4);
+    private GroundcoverType groundcoverType;
+
+    public BlockGroundCover(Material material, GroundcoverType type)
+    {
+        super(material);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(MODEL, 0));
+        this.groundcoverType = type;
+        this.setHardness(0F);
+    }
+
+    public GroundcoverType getGroundcoverType()
+    {
+        return this.groundcoverType;
+    }
+
+    @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
     {
         return new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D);
     }
-	
-	@Override
+
+    @Override
     @Nullable
     public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos)
     {
-		return null;
+        return null;
     }
 
-	@Override
+    @Override
     public boolean isPassable(IBlockAccess worldIn, BlockPos pos)
     {
         return true;
     }
-	
-	@Override
+
+    @Override
     public boolean causesSuffocation(IBlockState state)
     {
         return false;
     }
-	
-	@Override
+
+    @Override
     public boolean isFullCube(IBlockState state)
     {
         return false;
     }
-	
-	@Override
-	public boolean isOpaqueCube(IBlockState state)
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
     {
         return false;
     }
-	
-	@Override
+
+    @Override
     public boolean canPlaceBlockAt(World worldIn, BlockPos pos)
     {
-        return super.canPlaceBlockAt(worldIn, pos) ? this.canBlockStay(worldIn, pos) : false;
+        return super.canPlaceBlockAt(worldIn, pos) && this.canBlockStay(worldIn, pos);
     }
-	
-	@Override
+
+    @Override
     public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos)
     {
         if (!this.canBlockStay(worldIn, pos))
@@ -95,104 +90,70 @@ public class BlockGroundCover extends Block implements IPropertyHelper
             worldIn.setBlockToAir(pos);
         }
     }
-	
+
     private boolean canBlockStay(World world, BlockPos pos)
     {
         return world.isSideSolid(pos.down(), EnumFacing.UP);
     }
-    
+
     @Override
     public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face)
     {
         return BlockFaceShape.UNDEFINED;
     }
-    
+
     @Override
     public boolean isTopSolid(IBlockState state)
     {
         return false;
     }
-    
+
     @Override
     public void onBlockAdded(World world, BlockPos pos, IBlockState state)
     {
-		if(!world.isRemote)
-		{
-			int meta = new Random().nextInt(5);
-			world.setBlockState(pos, this.getStateFromMeta(meta));
-		}
+        if (!world.isRemote)
+        {
+            int meta = new Random().nextInt(5);
+            world.setBlockState(pos, this.getStateFromMeta(meta));
+        }
     }
-	
-	@Override
+
+    @Override
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
     {
-		if(world.isRemote)
-		{
-			return true;
-		}
-		else
-		{
-			if(player.isSneaking())
-			{
-				ItemStack stack = new ItemStack(this, 1, 0);
-				InventoryHelper.spawnItemStack(world, pos.getX(), pos.getY(), pos.getZ(), stack);
-				world.setBlockToAir(pos);
-			}
-			else
-			{
-				if(player.isCreative())
-				{
-			        int meta = getMetaFromState(state);
-			        if(meta != 4)
-			        	meta++;
-			        else
-			        	meta = 0;
-			        
-			        world.setBlockState(pos, getStateFromMeta(meta));
-				}
-			}
-		}
-        
-        return true;
+        if (player.isCreative())
+        {
+            if (!world.isRemote) world.setBlockState(pos, getStateFromMeta((getMetaFromState(state) + 1) % 5));
+            return true;
+        }
+
+        return false;
     }
-	
-	@Override
+
+    @Override
     public boolean canHarvestBlock(IBlockAccess world, BlockPos pos, EntityPlayer player)
     {
         return true;
     }
-	
-	@Override
+
+    @Override
     public boolean isReplaceable(IBlockAccess worldIn, BlockPos pos)
     {
-        return true;
+        return false;
     }
-	
-	@Override
-    public Item getItemDropped(IBlockState state, Random rand, int fortune)
-    {
-		if(this.getGroundcoverType() == GroundcoverType.TWIGS)
-		{
-			if(rand.nextBoolean())
-			{
-				return Items.STICK;
-			}
-		}
-        return Items.AIR;
-    }
-	
-	@Override
+
+    @Override
     public int damageDropped(IBlockState state)
     {
         return 0;
     }
 
-	@Override
+    @Override
     public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state)
     {
         return new ItemStack(this);
     }
-	
+
     @Override
     public IBlockState getStateFromMeta(int meta)
     {
@@ -202,27 +163,27 @@ public class BlockGroundCover extends Block implements IPropertyHelper
     @Override
     public int getMetaFromState(IBlockState state)
     {
-    	return state.getValue(MODEL).intValue();
+        return state.getValue(MODEL);
     }
 
     @Override
     protected BlockStateContainer createBlockState()
     {
-        return new BlockStateContainer(this, new IProperty[] {MODEL});
+        return new BlockStateContainer(this, MODEL);
     }
-    
-	@Override
-	public ImmutableList<IBlockState> getProperties()
-	{
-		return this.blockState.getValidStates();
-	}
-	
-	public static enum GroundcoverType
-	{
-		TWIGS,
-		ROCKS,
-		BONES,
-		SEASHELLS,
-		PINECONES;
-	}
+
+    @Override
+    public ImmutableList<IBlockState> getProperties()
+    {
+        return this.blockState.getValidStates();
+    }
+
+    public enum GroundcoverType
+    {
+        TWIGS,
+        ROCKS,
+        BONES,
+        SEASHELLS,
+        PINECONES;
+    }
 }
